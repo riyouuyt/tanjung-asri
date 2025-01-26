6925c1a9ac77d6c46953782df4bd998e5d12bf7d
Intan Saylindri <bagusariobimo7@gmail.com>
2025-01-27 01:48:31 +0700

    first commit

--- src/components/layout/Header/header.tsx
+++ src/components/layout/Header/header.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Link from "next/link";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/outline";
 import styles from "./header.module.css";
 import Image from "next/image";
